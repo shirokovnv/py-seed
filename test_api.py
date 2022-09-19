a02af574fc5b74220bef8096a7db07ab98d5cf99
shirokovnv <shirokovnv@gmail.com>
2022-09-20 00:52:26 +0500

    Fix tests

--- test_api.py
+++ test_api.py
@@ -4,7 +4,7 @@ Testing API.
 
 from fastapi.testclient import TestClient
 from api import app
-from schema import SCHEMA
+from schema import req_schema
 
 client = TestClient(app)
 
@@ -13,9 +13,9 @@ def test_read_seeds_ok():
     """
     Testing 200 response.
     """
-    response = client.post("/seeds", headers={"Content-type": "application/json"},
+    response = client.post('/seeds', headers={'Content-type': 'application/json'},
                            json={
-        "count": 1, "format": "json", "schema": SCHEMA}
+        'count': 1, 'format': 'json', 'schema': req_schema}
     )
     assert response.status_code == 200
 
@@ -24,8 +24,8 @@ def test_read_seeds_unprocessable_entity():
     """
     Testing 422 response.
     """
-    response = client.post("/seeds", headers={"Content-type": "application/json"},
+    response = client.post('/seeds', headers={'Content-type': 'application/json'},
                            json={
-        "count": 1, "format": "json", "schema": {}}
+        'count': 1, 'format': 'json', 'schema': {}}
     )
     assert response.status_code == 422
